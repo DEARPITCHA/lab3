bce3d06c04c68dcd4d1484b302093475ea35de0f
lab303-17 <lab303-17@labeng.local>
2019-01-11 17:23:55 +0700

    -333

---
+++ lab3_3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double x=1,y=2;
+    double sum ;
+    while (y<=69){
+        sum+=x/y;
+       y=y+1;
+    }
+    cout<< sum; 
+    
+    return 0;
+}
